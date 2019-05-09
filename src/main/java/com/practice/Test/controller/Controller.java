6c582902482636120f501ca812ee5d9ebe439c07
filiboy03 <filiboy03@gmail.com>
2019-05-09 15:11:12 -0500

    update and delete api added

--- src/main/java/com/practice/Test/controller/Controller.java
+++ src/main/java/com/practice/Test/controller/Controller.java
@@ -7,9 +7,12 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -58,5 +61,31 @@ public class Controller {
 	public Note getNote(@PathVariable(value="id") Long noteid) {
 	   
 		return noteRepository.findById(noteid).orElseThrow(()-> new ResourceNotFoundException("Note","id",noteid));
+	}
+	
+	// Update a Note
+	@PutMapping("/notes/{id}")
+	public Note updateNote(@PathVariable(value = "id") Long noteId,
+	                                        @Valid @RequestBody Note noteDetails) {
+
+	    Note note = noteRepository.findById(noteId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+
+	    note.setTitle(noteDetails.getTitle());
+	    note.setContent(noteDetails.getContent());
+
+	    Note updatedNote = noteRepository.save(note);
+	    return updatedNote;
+	}
+	
+	// Delete a Note
+	@DeleteMapping("/notes/{id}")
+	public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long noteId) {
+	    Note note = noteRepository.findById(noteId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+
+	    noteRepository.delete(note);
+
+	    return ResponseEntity.ok().build();
 	}
 }
